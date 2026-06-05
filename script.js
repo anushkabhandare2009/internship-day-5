83eb8d25695ee75d39d3da1f050cdeb66c1515a0
anushkabhandare2009 <anushkabhandare2009@gmail.com>
2026-06-05 10:20:33 +0530

    day 5 work

---
+++ script.js
@@ -0,0 +1,25 @@
+window.alert("Hello World!");
+
+let name = "Anushka";
+
+console.log("hello", name , "welcome");
+
+let num1= 5;
+let num2= 15;
+
+console.log("number 1 is", num1);
+console.log("number 2 is", num2);
+
+console.log("the sum is", num1 + num2);
+console.log("subtraction is", num1 - num2);
+console.log("multiplication is", num1 * num2);
+console.log("division is", num1 / num2);
+
+console.log("is equal",num1 == num2);
+console.log("is not equal",num1 != num2);
+console.log("is greater than",num1 > num2);
+console.log("is less than",num1 < num2);
+
+console.log("logical and", num1 > 0 && num2 > 0);
+console.log("logical or", num1 > 0 || num2 > 0);
+console.log("logical not", !(num1 > 0));
